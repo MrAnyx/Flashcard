2e744cdbd178334ba11f122efea703fe876b11f9
MrAnyx <gentle.job6107@fastmail.com>
2025-01-04 15:59:23 +0000

    fix: Update environment variable handling in Supervisor configuration scripts

--- .docker/entrypoint.prod.sh
+++ .docker/entrypoint.prod.sh
@@ -6,16 +6,17 @@ echo "Running database migrations..."
 php bin/console doctrine:migrations:migrate --no-interaction --allow-no-migration
 
 # Generate Supervisor configuration
+
+
 echo "Generating Supervisor configuration..."
 SUPERVISORD_CONF="/etc/supervisor/conf.d/supervisord.conf"
-ENV_VARS=$(printenv | awk -F= '{print $1"=\""$2"\""}' | tr '\n' ',' | sed 's/,$//')
-ESCAPED_ENV_VARS=$(echo "$ENV_VARS" | sed 's/"/\\"/g')
+ENV_VARS=$(printenv | awk -F= '{print $1"=\""$2"\","}' | tr -d '\n' | sed 's/,$//')
 echo "" >> $SUPERVISORD_CONF
 echo "[supervisord]" >> $SUPERVISORD_CONF
 echo "nodaemon=true" >> $SUPERVISORD_CONF
 echo "logfile=/dev/null" >> $SUPERVISORD_CONF
 echo "logfile_maxbytes=0" >> $SUPERVISORD_CONF
-echo "environment=${ESCAPED_ENV_VARS}" >> $SUPERVISORD_CONF
+echo "environment=${ENV_VARS}" >> $SUPERVISORD_CONF
 
 # Update and start supervisor service
 echo "Starting supervisor..."
