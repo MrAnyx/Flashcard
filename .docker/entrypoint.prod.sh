6677a0890a3fb5ad2b5376c8e0ec251ece770fe0
MrAnyx <gentle.job6107@fastmail.com>
2025-01-04 12:20:10 +0000

    fix: Migrate from S6-Overlay to Supervisor for process management and update entrypoint scripts

--- .docker/entrypoint.prod.sh
+++ .docker/entrypoint.prod.sh
@@ -5,6 +5,16 @@ set -e
 echo "Running database migrations..."
 php bin/console doctrine:migrations:migrate --no-interaction --allow-no-migration
 
+cat /etc/environment \
+    | sed -e 's/"//g' \
+    | awk -F= '{key=$1}{value=$2} BEGIN {printf "environment="} NR>1 {printf ","} {printf key"=\42"value"\42"} END {printf "\n"}' \
+    >> /etc/supervisor/conf.d/supervisord.conf
+
 # Update and start supervisor service
-echo "Starting S6-Overlay..."
-exec /init
+echo "Starting supervisor..."
+service supervisor start
+supervisorctl reread
+supervisorctl update
+supervisorctl restart all
+
+exec docker-php-entrypoint "$@"
