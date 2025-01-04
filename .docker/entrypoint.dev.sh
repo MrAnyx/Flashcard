e3b3bb875c6bdf848a7dd3099a39811de6125a8b
MrAnyx <gentle.job6107@fastmail.com>
2025-01-04 12:25:04 +0000

    fix: Remove environment variable sourcing from entrypoint scripts and update supervisord command

--- .docker/entrypoint.dev.sh
+++ .docker/entrypoint.dev.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 set -e
 
-cat /etc/environment \
-    | sed -e 's/"//g' \
-    | awk -F= '{key=$1}{value=$2} BEGIN {printf "environment="} NR>1 {printf ","} {printf key"=\42"value"\42"} END {printf "\n"}' \
-    >> /etc/supervisor/conf.d/supervisord.conf
-
 # Update and start supervisor service
 echo "Starting supervisor..."
 service supervisor start
--- .docker/entrypoint.prod.sh
+++ .docker/entrypoint.prod.sh
@@ -5,11 +5,6 @@ set -e
 echo "Running database migrations..."
 php bin/console doctrine:migrations:migrate --no-interaction --allow-no-migration
 
-cat /etc/environment \
-    | sed -e 's/"//g' \
-    | awk -F= '{key=$1}{value=$2} BEGIN {printf "environment="} NR>1 {printf ","} {printf key"=\42"value"\42"} END {printf "\n"}' \
-    >> /etc/supervisor/conf.d/supervisord.conf
-
 # Update and start supervisor service
 echo "Starting supervisor..."
 service supervisor start
