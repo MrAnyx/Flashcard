30b0a885a1729cf3cc5af8584ecf2629ab44a193
MrAnyx <gentle.job6107@fastmail.com>
2025-01-04 15:37:35 +0000

    feat: Generate Supervisor configuration in entrypoint scripts and add configuration script

--- .docker/entrypoint.prod.sh
+++ .docker/entrypoint.prod.sh
@@ -5,11 +5,19 @@ set -e
 echo "Running database migrations..."
 php bin/console doctrine:migrations:migrate --no-interaction --allow-no-migration
 
+# Generate Supervisor configuration
+echo "Generating Supervisor configuration..."
+SUPERVISORD_CONF="/etc/supervisor/conf.d/supervisord.conf"
+ENV_VARS=$(printenv | sed 's/^\(.*\)$/\1/' | tr '\n' ',' | sed 's/,$//')
+echo "[supervisord]" >> $SUPERVISORD_CONF
+echo "nodaemon=true" >> $SUPERVISORD_CONF
+echo "logfile=/dev/null" >> $SUPERVISORD_CONF
+echo "logfile_maxbytes=0" >> $SUPERVISORD_CONF
+echo "environment=${ENV_VARS}" >> $SUPERVISORD_CONF
+
 # Update and start supervisor service
 echo "Starting supervisor..."
 service supervisor start
 supervisorctl reread
 supervisorctl update
 supervisorctl restart all
-
-exec docker-php-entrypoint "$@"
--- .docker/supervisord/generate-supervisord-config.sh
+++ .docker/supervisord/generate-supervisord-config.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
-set -e
 
-echo "Generating Supervisor configuration..."
+# Original supervisord configuration template
 SUPERVISORD_CONF="/etc/supervisor/conf.d/supervisord.conf"
+
+# Get all host environment variables and format them for supervisord
 ENV_VARS=$(printenv | sed 's/^\(.*\)$/\1/' | tr '\n' ',' | sed 's/,$//')
-echo "" >> $SUPERVISORD_CONF
+
+# Create the final supervisord configuration
 echo "[supervisord]" >> $SUPERVISORD_CONF
 echo "nodaemon=true" >> $SUPERVISORD_CONF
 echo "logfile=/dev/null" >> $SUPERVISORD_CONF
 echo "logfile_maxbytes=0" >> $SUPERVISORD_CONF
 echo "environment=${ENV_VARS}" >> $SUPERVISORD_CONF
-
-# Update and start supervisor service
-echo "Starting supervisor..."
-service supervisor start
-supervisorctl reread
-supervisorctl update
-supervisorctl restart all
