6dc2fd9d3b46fa8746cc2dd6f5d29f39d6b2b62c
MrAnyx <gentle.job6107@fastmail.com>
2025-01-04 15:46:39 +0000

    fix: Escape environment variables in Supervisor configuration scripts

--- .docker/entrypoint.dev.sh
+++ .docker/entrypoint.dev.sh
@@ -3,13 +3,14 @@ set -e
 
 echo "Generating Supervisor configuration..."
 SUPERVISORD_CONF="/etc/supervisor/conf.d/supervisord.conf"
-ENV_VARS=$(printenv | sed 's/^\(.*\)$/\1/' | tr '\n' ',' | sed 's/,$//')
+ENV_VARS=$(printenv | awk -F= '{print $1"=\""$2"\""}' | tr '\n' ',' | sed 's/,$//')
+ESCAPED_ENV_VARS=$(echo "$ENV_VARS" | sed 's/"/\\"/g')
 echo "" >> $SUPERVISORD_CONF
 echo "[supervisord]" >> $SUPERVISORD_CONF
 echo "nodaemon=true" >> $SUPERVISORD_CONF
 echo "logfile=/dev/null" >> $SUPERVISORD_CONF
 echo "logfile_maxbytes=0" >> $SUPERVISORD_CONF
-echo "environment=${ENV_VARS}" >> $SUPERVISORD_CONF
+echo "environment=${ESCAPED_ENV_VARS}" >> $SUPERVISORD_CONF
 
 # Update and start supervisor service
 echo "Starting supervisor..."
